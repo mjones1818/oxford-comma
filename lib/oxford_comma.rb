d8342b75eb89cc061469513511b0b5eb1438fb03
mjones1818 <michael.jones0532@gmail.com>
2020-10-01 12:34:46 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0].to_s
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array[0] + ", " + array[1] + ", " + "and " + array[2] 
+  else
+    last_name = array.pop
+    array.join(", ") + ","" and " + last_name
+  end
 end
